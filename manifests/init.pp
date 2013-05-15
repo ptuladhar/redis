ae2b62471b7312df1ae6b27cdef0c39dc9cf5816
Vagrant <vagrant@vagrant-debian-squeeze-64.vagrantup.com>
2013-05-15 07:47:50 +0200

    added manifests.

---
+++ manifests/init.pp
@@ -0,0 +1,8 @@
+" ============================================================================
+" Netrw Directory Listing                                        (netrw v138)
+"   /home/vagrant/retailapp-infra-repos/redis/manifests
+"   Sorted by      name
+"   Sort sequence: [\/]$,\<core\%(\.\d\+\)\=\>,\.h$,\.c$,\.cpp$,*,\.o$,\.obj$,\.info$,\.swp$,\.bak$,\~$
+"   Quick Help: <F1>:help  -:go up dir  D:delete  R:rename  s:sort-by  x:exec
+" ============================================================================
+../
